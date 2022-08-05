54d1daa1730c385d1d30d501234b80899c46a308
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-08-05 16:32:17 -0300

    feat: connect to wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+  // Use o hook connectWallet que o thirdweb nos dá.
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+
+  // Esse é o caso em que o usuário ainda não conectou sua carteira
+  // ao nosso webapp. Deixe ele chamar connectWallet.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Bem-vind@s à OssDAO - a DAO dos jiujiteiros</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Conecte sua carteira
+        </button>
+      </div>
+    );
+  }
+  
+  // Esse é o caso em que temos o endereço do usuário
+  // o que significa que ele conectou sua carteira ao nosso site!
   return (
     <div className="landing">
-      <h1>Bem-vind@s à minha DAO</h1>
-    </div>
-  )
-}
+      <h1>👀 carteira conectada, e agora?!</h1>
+    </div>);
+};
 
-export default App
+export default App;
