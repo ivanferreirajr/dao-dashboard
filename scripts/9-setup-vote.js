8c5a48bdeb05ca03b9187189fb79b111415fb71b
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-08-07 18:53:56 -0300

    feat(script): creation of the dao treasure

---
+++ scripts/9-setup-vote.js
@@ -0,0 +1,45 @@
+import sdk from "./1-initialize-sdk.js";
+
+// Esse é o nosso contrato de governança.
+const vote = sdk.getVote("0x627Fc7Ac08Aa449dF4Ca91421613F8B2394194cf");
+
+// Esse é o nosso contrato ERC-20.
+const token = sdk.getToken("0xfD51e526F4E1a51e07D92857BD65c25931fCbf51");
+
+(async () => {
+  try {
+    // Dê para a nosso tesouro o poder de cunhar tokens adicionais se necessário.
+    await token.roles.grant("minter", vote.getAddress());
+
+    console.log(
+      "✅  Módulo de votos recebeu permissão de manipular os tokens com sucesso"
+    );
+  } catch (error) {
+    console.error(
+      "falha ao dar acesso aos tokens ao módulo de votos",
+      error
+    );
+    process.exit(1);
+  }
+
+  try {
+    //Pegue o saldo de tokens da nossa carteira, lembre-se -- nós detemos basicamente o fornecimento inteiro agora!
+    const ownedTokenBalance = await token.balanceOf(
+      process.env.WALLET_ADDRESS
+    );
+
+    // Pegue 90% do fornecimento que nós detemos.
+    const ownedAmount = ownedTokenBalance.displayValue;
+    const percent90 = Number(ownedAmount) / 100 * 90;
+
+    // Transfira 90% do fornecimento para nosso contrato de votação.
+    await token.transfer(
+      vote.getAddress(),
+      percent90
+    ); 
+
+    console.log("✅ Transferiu " + percent90 + " tokens para o módulo de votos com sucesso");
+  } catch (err) {
+    console.error("falhar ao transferir tokens ao módulo de votos", err);
+  }
+})();
