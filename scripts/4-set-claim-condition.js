db18a55c7e88bc06db54ae9bbeef441afef5f590
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2022-08-07 10:53:02 -0300

    feat(script): set claim condition

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,29 @@
+import sdk from "./1-initialize-sdk.js";
+import { MaxUint256 } from "@ethersproject/constants";
+
+const editionDrop = sdk.getEditionDrop("0xEf0e209B614fc2123274A66748f9d4aec58cEAD2");
+
+(async () => {
+  try {
+    // Especifique as condições.
+    const claimConditions = [{
+      // Quando as pessoas vão poder reivindicar seus NFTs
+      startTime: new Date(),
+      // Número máximo de NFTs
+      maxQuantity: 50_000,
+      // o preço do NFT (grátis)
+      price: 0,
+      // Quantos NFTs podem ser reivindicados por transação.
+      quantityLimitPerTransaction: 1,
+      // tempo de espera entre transações infinito significa que cada
+      // pessoa só pode solicitar um único NFT.
+      waitInSeconds: MaxUint256,
+    }]
+    
+    await editionDrop.claimConditions.set("0", claimConditions);
+
+    console.log("✅ Condições de reinvidicação configuradas com sucesso!");
+  } catch (error) {
+    console.error("Falha ao definir condições de reinvidicação", error);
+  }
+})()
